12efa06921b71d91bb7d2b3c42d998f212c63d84
Ali Roemhildt <aliroemhildt@gmail.com>
2021-10-31 01:22:53 -0600

    Complete lets cook button and clear button functionality

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
 var allButtons = document.querySelectorAll('.radio');
+var sideRadioButton = document.querySelector('input[value="side"]');
+var mainDishRadioButton = document.querySelector('input[value="main-dish"]');
+var dessertRadioButton = document.querySelector('input[value="dessert"]');
 var letsCookButton = document.querySelector('.lets-cook-button');
 var recipeView = document.querySelector('.recipe-view');
 var homeView = document.querySelector('.home-view');
 var recipeName = document.querySelector('.recipe-text');
+var clearButton = document.querySelector('.clear-button');
+
 var selectedValue;
 var recipeToDisplay;
 
 letsCookButton.addEventListener('click', displayRecipe);
+sideRadioButton.addEventListener('click', selectRecipeType);
+mainDishRadioButton.addEventListener('click', selectRecipeType);
+dessertRadioButton.addEventListener('click', selectRecipeType);
+clearButton.addEventListener('click', clearSelection);
 
 function displayRecipe() {
   checkRadioButtons();
   chooseRecipe();
-  addHidden(homeView);
-  removeHidden(recipeView);
-  recipeName.innerText = recipeToDisplay;
+  if (recipeToDisplay) {
+    addHidden(homeView);
+    removeHidden(recipeView);
+    recipeName.innerText = recipeToDisplay;
+  };
+};
+
+function selectRecipeType() {
+  checkRadioButtons();
+  if (selectedValue) {
+    removeHidden(letsCookButton);
+  };
+};
+
+function clearSelection() {
+  deselectRadioButtons();
+  addHidden(recipeView);
+  removeHidden(homeView);
+  addHidden(letsCookButton);
 };
 
 function checkRadioButtons() {
@@ -26,12 +51,22 @@ function checkRadioButtons() {
 
 function chooseRecipe() {
   if (selectedValue === 'side') {
-    recipeToDisplay = sides[getRandomIndex(sides)];
+    newRecipeToDisplay = sides[getRandomIndex(sides)];
+    if (newRecipeToDisplay === recipeToDisplay) {
+      newRecipeToDisplay = sides[getRandomIndex(sides)];
+    };
   } else if (selectedValue === 'main-dish') {
-    recipeToDisplay = mainDishes[getRandomIndex(sides)];
+    newRecipeToDisplay = mainDishes[getRandomIndex(sides)];
+    if (newRecipeToDisplay === recipeToDisplay) {
+      newRecipeToDisplay = mainDishes[getRandomIndex(sides)];
+    };
   } else if (selectedValue === 'dessert') {
-    recipeToDisplay = desserts[getRandomIndex(sides)];
+    newRecipeToDisplay = desserts[getRandomIndex(sides)];
+    if (newRecipeToDisplay === recipeToDisplay) {
+      newRecipeToDisplay = desserts[getRandomIndex(sides)];
+    };
   };
+  recipeToDisplay = newRecipeToDisplay;
 };
 // need to refactor, but not sure how...
 //   var values = ['side', 'main-dish', 'dessert']
@@ -44,6 +79,12 @@ function chooseRecipe() {
 //   var recipe = ${recipeType[getRandomIndex(sides)]};
 //   return recipe;
 // };
+
+function deselectRadioButtons() {
+  for (var i = 0; i < allButtons.length; i++) {
+    allButtons[i].checked = false;
+  };
+};
 
 function addHidden(element) {
   element.classList.add('hidden');
